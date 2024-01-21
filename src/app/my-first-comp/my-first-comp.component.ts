43a18ac378ce37caf9e25095ebab18a7981862b2
silvano.cirasa <silvano.cirasa@students.ffhs.ch>
2024-01-22 00:35:16 +0100

    form component

---
+++ src/app/my-first-comp/my-first-comp.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-first-comp',
+  standalone: true,
+  imports: [],
+  templateUrl: './my-first-comp.component.html',
+  styleUrl: './my-first-comp.component.css'
+})
+export class MyFirstCompComponent {
+
+}
