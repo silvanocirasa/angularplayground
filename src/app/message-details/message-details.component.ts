a8c93907ff3ed196a03baeaf3f9d46eb4191cf4a
silvano.cirasa <silvano.cirasa@students.ffhs.ch>
2024-01-22 01:57:44 +0100

    share data between two components

---
+++ src/app/message-details/message-details.component.ts
@@ -0,0 +1,23 @@
+import {Component, EventEmitter, Input, NO_ERRORS_SCHEMA, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-message-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './message-details.component.html',
+  styleUrl: './message-details.component.css'
+})
+export class MessageDetailsComponent {
+
+  @Input()
+  message: any = {};
+  @Input()
+  index: number = -1;
+
+  @Output()
+  delete: EventEmitter<number> = new EventEmitter<number>();
+
+  onDelete() {
+    this.delete.emit(this.index);
+  }
+}
--- src/app/my-first-comp/my-first-comp.component.ts
+++ src/app/my-first-comp/my-first-comp.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import {FormsModule} from "@angular/forms";
+import {NgForOf, NgIf} from "@angular/common";
+import {MessageDetailsComponent} from "../message-details/message-details.component";
 
 @Component({
   selector: 'app-my-first-comp',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    NgIf,
+    NgForOf,
+    MessageDetailsComponent
+  ],
   templateUrl: './my-first-comp.component.html',
   styleUrl: './my-first-comp.component.css'
 })
 export class MyFirstCompComponent {
 
+  name: string = '';
+  email: string = '';
+  message: string = '';
+  isSubmitted = false;
+  messages: Array<any> = [];
+  onSubmit(){
+    this.isSubmitted = true;
+    this.messages.push({
+      'name': this.name,
+      'email': this.email,
+      'message': this.message
+    });
+    console.log(this.messages);
+  }
+
+  deleteMessage(index: number) {
+    this.messages.splice(index, 1);
+  }
 }
